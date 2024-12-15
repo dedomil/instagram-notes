d4a23818e1e8c74ebdad7a9a84f5f78ca8851f3f
Aditya Yadav <dedomil@tuta.io>
2024-12-15 20:31:57 +0530

    add getSessionId function and fix #1

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
 import axios, { RawAxiosRequestHeaders, Method } from "axios";
-import { baseURL, defaultHeaders, loginURL } from "./config.json";
+import qs from "query-string";
+import {
+  baseURL,
+  defaultHeaders,
+  loginURL,
+  fetchHeaderURL,
+  loginHeaders,
+} from "./config.json";
 
 class Note {
   sessionId: string;
@@ -8,8 +15,30 @@ class Note {
     this.sessionId = `sessionid=${sessionId}`;
   }
 
-  async formatCookie(setCookie: string[] | undefined) {
+  private static formatCookie(setCookie: string[] | undefined) {
     return setCookie?.map((x) => x.match(/(.*?=.*?);/)?.[1])?.join("; ");
+  }
+
+  static async getSessionId(username: string, password: string) {
+    const fetchHeaders = (await axios.get(fetchHeaderURL)).headers;
+    const cookie = this.formatCookie(fetchHeaders["set-cookie"]) || "";
+
+    const payload = qs.stringify({
+      username,
+      password: password,
+      device_id: crypto.randomUUID(),
+      login_attempt_count: 0,
+    });
+
+    const { headers } = await axios.post(loginURL, payload, {
+      headers: { ...loginHeaders, cookie },
+    });
+
+    const sessionIdCookie = headers["set-cookie"]?.find((e) =>
+      e.startsWith("sessionid=")
+    );
+
+    return sessionIdCookie?.match(/sessionid=([A-Za-z0-9%_]+)/)?.[1] || "";
   }
 
   async privateRequest(
